f692b7ef67c203584a42e25263440c5470749035
Anita Dijala <dijalaanita@gmail.com>
2026-07-06 17:06:48 +0100

    CTEs class completed

---
+++ CTEs.sql
@@ -0,0 +1,51 @@
+# CTEs COMMON TABLE EXPRESSIONS
+# allows you to define a separate block that you can reference in the main queries
+
+WITH CTE_Example AS
+(
+SELECT gender, AVG(salary) AS avg_sal, MAX(salary) AS max_sal, MIN(salary) AS min_sal, COUNT(salary) AS count_sal
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender)
+SELECT AVG(avg_sal)
+FROM CTE_Example
+;
+
+SELECT AVG(avg_sal)
+FROM (SELECT gender, AVG(salary) AS avg_sal, MAX(salary) AS max_sal, MIN(salary) AS min_sal, COUNT(salary) AS count_sal
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender) AS eg_sub
+;
+
+WITH CTE_Example AS
+(
+SELECT employee_id, gender, birth_date
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+),
+CTE_Ex2 AS
+(
+SELECT employee_id, salary
+FROM employee_salary
+WHERE salary > 50000
+)
+SELECT *
+FROM CTE_Example
+JOIN CTE_Ex2
+	ON CTE_Example.employee_id = CTE_Ex2.employee_id
+;
+
+# changing column variables a different way than the 'AS' method
+WITH CTE_Example (gender, avg_sal, max_sal, min_sal, count_sal) AS
+(
+SELECT gender, AVG(salary), MAX(salary), MIN(salary), COUNT(salary)
+FROM employee_demographics AS dem
+JOIN employee_salary AS sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender)
+SELECT *
+FROM CTE_Example
+;
